709503086b1942671055736343f112f65a0ba532
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-25 17:14:26 +0100

    Importato google font e fontawesome

--- index.php
+++ index.php
@@ -60,6 +60,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-google-faq</title>
     <link rel="stylesheet" href="css/style.css">
+
+    <!-- Link Fontawesome 5 cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Link per Google Fonts - Roboto - Open Sans -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
 </head>
 <body>
 
