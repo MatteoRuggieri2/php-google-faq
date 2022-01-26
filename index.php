7a5a6c8f29e6bdbf670688527b0fc1e0ec91c2f2
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-26 15:18:19 +0100

    Sistemato font della ol

--- index.php
+++ index.php
@@ -27,8 +27,8 @@
                     <li>
                         <p>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:</p>
                         <ol class="letters-style">
-                            <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
-                            <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
+                            <li><p>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</p></li>
+                            <li><p>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</p></li>
                         </ol>
                     </li>
                     <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
